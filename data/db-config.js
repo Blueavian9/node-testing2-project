af6745538ba601942b4f47d0bd27dd16a7e1521b
Cesar Aguilar <Blueavian9@gmail.com>
2023-04-29 01:44:59 -0700

    Updated dv config and dependencies

--- data/db-config.js
+++ data/db-config.js
@@ -1,5 +1,6 @@
+/*  eslint-disable no-undef */
 const knex = require("knex");
-const NODE_ENV = process.env.NODE_ENV || 9000;
-const config = require(`../knexfile`);
+const NODE_ENV = process.env.NODE_ENV || "development";
+const config = require(".//knexfile");
 
 module.exports = knex(config[NODE_ENV]);
